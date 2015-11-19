e00c79a80a6b27aff36b9698508e6b66b0442865
Brax <baf9201@rit.edu>
2015-11-19 13:11:26 -0500

    Added BoardService::getUsers/Admins/PendingUsers, IdeaCollectionService::getIdeaCollections, and IdeaService::getIdeas

--- api/services/BoardService.js
+++ api/services/BoardService.js
@@ -17,6 +17,27 @@ boardService.destroy = function(boardId) {
   return Board.model.remove({boardId: boardId});
 };
 
+// find users
+boardService.getUsers = function(boardId) {
+  return Board.model.findOne({boardId: boardId})
+  .populate('users', '-_id')
+  .exec((board) => board.users);
+};
+
+// find admins
+boardService.getAdmins = function(boardId) {
+  return Board.model.findOne({boardId: boardId})
+  .populate('admins', '-_id')
+  .exec((board) => board.admins);
+};
+
+// find pending users
+boardService.getPendingUsers = function(boardId) {
+  return Board.model.findOne({boardId: boardId})
+  .populate('pendingUsers', '-_id')
+  .exec((board) => board.pendingUsers);
+};
+
 // Add a user to the board
 boardService.addUser = function(boardId, userId) {
 
